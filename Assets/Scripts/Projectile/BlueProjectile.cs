36798b1291995cce84b1e80dd2ed6e568b18f856
RafliIhsanuddin <93479421+RafliIhsanuddin@users.noreply.github.com>
2025-12-24 13:09:53 +0700

    feat(character) : tambah karakter dan animasi menembak kapsul
    TODO : implementasi mekanik menembak projectile dari karakter

---
+++ Assets/Scripts/Projectile/BlueProjectile.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BlueProjectile : MonoBehaviour
+{
+    public int damage;
+
+    public float speed = 0.0f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position += new Vector3(speed * Time.fixedDeltaTime, 0f, 0f);
+    }
+}
