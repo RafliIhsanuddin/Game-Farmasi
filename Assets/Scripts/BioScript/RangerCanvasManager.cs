7193876543f76bec0d1fc1c42ff418edf3d489b4
RafliIhsanuddin <93479421+RafliIhsanuddin@users.noreply.github.com>
2026-01-22 09:00:37 +0700

    feat(bio-ranger): add button to open bio ranger panel
    next: implement save flag at end of level

---
+++ Assets/Scripts/BioScript/RangerCanvasManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class RangerCanvasManager : MonoBehaviour
+{
+    [Header("All Canvases")]
+    [SerializeField] private GameObject defaultCanvas;
+    [SerializeField] private GameObject whiteCanvas;
+    [SerializeField] private GameObject blueCanvas;
+    [SerializeField] private GameObject pinkCanvas;
+    [SerializeField] private GameObject redCanvas;
+    [SerializeField] private GameObject greenCanvas;
+    [SerializeField] private GameObject yellowCanvas;
+
+    private GameObject[] allCanvases;
+
+    private void Start()
+    {
+        allCanvases = new GameObject[]
+        {
+            defaultCanvas,
+            whiteCanvas,
+            blueCanvas,
+            pinkCanvas,
+            redCanvas,
+            greenCanvas,
+            yellowCanvas
+        };
+
+        ShowDefaultCanvas(); // default saat Start
+    }
+
+    private void ShowOnly(GameObject canvasToShow)
+    {
+        foreach (var canvas in allCanvases)
+        {
+            if (canvas != null)
+                canvas.SetActive(canvas == canvasToShow);
+        }
+    }
+
+    public void ShowDefaultCanvas() => ShowOnly(defaultCanvas);
+    public void ShowWhiteCanvas()   => ShowOnly(whiteCanvas);
+    public void ShowBlueCanvas()    => ShowOnly(blueCanvas);
+    public void ShowPinkCanvas()    => ShowOnly(pinkCanvas);
+    public void ShowRedCanvas()     => ShowOnly(redCanvas);
+    public void ShowGreenCanvas()   => ShowOnly(greenCanvas);
+    public void ShowYellowCanvas()  => ShowOnly(yellowCanvas);
+}
